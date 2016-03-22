5bb933e6c892eee98607fd6ccf9766380fcc963e
Pavel Petrov <petrov.pavel.89@gmail.com>
2016-03-22 21:13:56 +0500

    Implementation of the basic rules.

---
+++ src/com/ppetrov/game/model/Map.java
@@ -0,0 +1,137 @@
+package com.ppetrov.game.model;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by Pavel on 22.03.2016.
+ */
+public class Map {
+
+    private boolean[][] field;
+
+    public Map(boolean[][] field) {
+        this.field = deepArrayCopy(field);
+    }
+
+    public Map(int width, int height) {
+        this.field = new boolean[width][height];
+        fillRandomly();
+    }
+
+    public boolean isEmpty() {
+        for (boolean[] row : this.field) {
+            for (boolean cell : row) {
+                if (cell) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void nextState() {
+        boolean[][] prevStateField = deepArrayCopy(this.field);
+
+        for (int i = 0; i < prevStateField.length; i++) {
+            for (int j = 0; j < prevStateField[i].length; j++) {
+                int aliveNeighbours = countOfAliveNeighbours(prevStateField, i, j);
+                if (aliveNeighbours < 2 || 3 < aliveNeighbours) {
+                    setCell(i, j, false);
+                } else if (aliveNeighbours == 3 && !this.field[i][j]) {
+                    setCell(i, j, true);
+                }
+            }
+        }
+    }
+
+    public boolean getCell(int row, int column) {
+        return getCell(this.field, row, column);
+    }
+
+    private boolean getCell(boolean[][] field, int row, int column) {
+        row = fixRow(row);
+        column = fixColumn(row, column);
+
+        return field[row][column];
+    }
+
+    public boolean[][] getField() {
+        boolean[][] copy = new boolean[this.field.length][];
+        return deepArrayCopy(this.field);
+    }
+
+    private void fillRandomly() {
+        Random random = new Random();
+        for (boolean[] row : this.field) {
+            Arrays.fill(row, random.nextBoolean());
+        }
+    }
+
+    private int countOfAliveNeighbours(boolean[][] field, int row, int column) {
+        int count = 0;
+
+        if (getCell(field, row - 1, column - 1)) {
+            count++;
+        }
+        if (getCell(field, row - 1, column)) {
+            count++;
+        }
+        if (getCell(field, row - 1, column + 1)) {
+            count++;
+        }
+        if (getCell(field, row, column - 1)) {
+            count++;
+        }
+        if (getCell(field, row, column + 1)) {
+            count++;
+        }
+        if (getCell(field, row + 1, column - 1)) {
+            count++;
+        }
+        if (getCell(field, row + 1, column)) {
+            count++;
+        }
+        if (getCell(field, row + 1, column + 1)) {
+            count++;
+        }
+
+        return count;
+    }
+
+    private void setCell(int row, int column, boolean value) {
+        row = fixRow(row);
+        column = fixColumn(row, column);
+
+        this.field[row][column] = value;
+    }
+
+    private int fixRow(int row) {
+        if (row < 0) {
+            row = this.field.length - 1;
+        }
+        if (row > this.field.length - 1) {
+            row = 0;
+        }
+        return row;
+    }
+
+    private int fixColumn(int row, int column) {
+        if (column < 0) {
+            column = this.field[row].length - 1;
+        }
+        if (column > this.field[row].length - 1) {
+            column = 0;
+        }
+        return column;
+    }
+
+    private boolean[][] deepArrayCopy(boolean[][] source) {
+        boolean[][] copy = new boolean[source.length][];
+        for (int i = 0; i < source.length; i++) {
+            copy[i] = Arrays.copyOf(source[i], source[i].length);
+        }
+        return copy;
+    }
+
+}
---
+++ test/com/ppetrov/game/model/MapTest.java
@@ -0,0 +1,61 @@
+package com.ppetrov.game.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Pavel on 22.03.2016.
+ */
+public class MapTest {
+
+    @Test
+    public void testIsEmpty() throws Exception {
+        Map map = new Map(new boolean[][]{{true, false}, {false, false}});
+        assertFalse(map.isEmpty());
+        map = new Map(new boolean[][]{{false, false}, {false, false}});
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void testNextState() throws Exception {
+        boolean[][] blockField = new boolean[][]{
+                {false, false, false, false},
+                {false, true, true, false},
+                {false, true, true, false},
+                {false, false, false, false}};
+        Map block = new Map(blockField);
+
+        block.nextState();
+        assertArrayEquals(blockField, block.getField());
+
+        boolean[][] blinkerField = new boolean[][]{
+                {false, false, false, false, false},
+                {false, false, false, false, false},
+                {false, true, true, true, false},
+                {false, false, false, false, false},
+                {false, false, false, false, false}
+        };
+        boolean[][] blinkerFieldNextStep = new boolean[][]{
+                {false, false, false, false, false},
+                {false, false, true, false, false},
+                {false, false, true, false, false},
+                {false, false, true, false, false},
+                {false, false, false, false, false}
+        };
+        Map blinker = new Map(blinkerField);
+
+        blinker.nextState();
+        assertArrayEquals(blinkerFieldNextStep, blinker.getField());
+        blinker.nextState();
+        assertArrayEquals(blinkerField, blinker.getField());
+    }
+
+    @Test
+    public void testGetCell() throws Exception {
+        Map map = new Map(new boolean[][]{{true, false}, {false, true}});
+        assertTrue(map.getCell(-1, -1));
+        assertTrue(map.getCell(2, 2));
+    }
+
+}
