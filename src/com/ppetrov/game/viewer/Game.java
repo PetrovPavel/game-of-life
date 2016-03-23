93fdff3939cfffb3f1cf141c8fb3ba92ea375a2b
Pavel Petrov <petrov.pavel.89@gmail.com>
2016-03-23 20:06:11 +0500

    Placed canvas onto scrollpane.

--- src/com/ppetrov/game/viewer/Game.java
+++ src/com/ppetrov/game/viewer/Game.java
@@ -3,10 +3,11 @@ package com.ppetrov.game.viewer;
 import com.ppetrov.game.model.Map;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -26,11 +27,14 @@ public class Game extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Game of Life");
 
+        ScrollPane scrollPane = new ScrollPane();
+
         Canvas canvas = new Canvas(500, 500);
         drawGame(canvas.getGraphicsContext2D());
+        scrollPane.setContent(canvas);
 
-        Group root = new Group();
-        root.getChildren().add(canvas);
+        HBox root = new HBox();
+        root.getChildren().add(scrollPane);
         primaryStage.setScene(new Scene(root));
 
         primaryStage.show();
