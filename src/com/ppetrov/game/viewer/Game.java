1c9fe6bff50215a51b25c82fab4beadf2b2bb794
Pavel Petrov <petrov.pavel.89@gmail.com>
2016-03-25 23:27:25 +0500

    Added possibility to specify width and height of the field.

--- src/com/ppetrov/game/viewer/Game.java
+++ src/com/ppetrov/game/viewer/Game.java
@@ -6,8 +6,12 @@ import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Spinner;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -19,6 +23,10 @@ import java.util.TimerTask;
  */
 public class Game extends Application {
 
+    private int width = 100;
+
+    private int height = 100;
+
     private Map map = new Map(100, 100);
 
     private Timer timer = new Timer();
@@ -27,18 +35,41 @@ public class Game extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Game of Life");
 
-        ScrollPane scrollPane = new ScrollPane();
+        ScrollPane canvasPane = new ScrollPane();
+        canvasPane.setStyle("-fx-background-color:transparent;");
 
         Canvas canvas = new Canvas(500, 500);
-        drawGame(canvas.getGraphicsContext2D());
-        scrollPane.setContent(canvas);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        drawGame(gc);
+        canvasPane.setContent(canvas);
+
+        Label widthLabel = new Label("Field width:");
+        Spinner<Integer> widthSpinner = new Spinner<>(1, 150, 100);
+        Label heightLabel = new Label("Field height:");
+        Spinner<Integer> heightSpinner = new Spinner<>(1, 150, 100);
+
+        Button applySettingsButton = new Button("Apply");
+        applySettingsButton.setMaxWidth(Integer.MAX_VALUE);
+        applySettingsButton.setOnAction(event -> {
+            this.width = widthSpinner.getValue();
+            this.height = heightSpinner.getValue();
+            this.map = new Map(this.width, this.height);
+            int cellSize = getCellSize();
+            canvas.setWidth(cellSize * this.width);
+            canvas.setHeight(cellSize * this.height);
+        });
+
+        VBox settingsGroup = new VBox(
+                widthLabel, widthSpinner,
+                heightLabel, heightSpinner,
+                applySettingsButton
+        );
+        settingsGroup.setStyle("-fx-background-color:transparent;");
 
         HBox root = new HBox();
-        root.getChildren().add(scrollPane);
-        root.setFillHeight(false);
+        root.getChildren().addAll(canvasPane, settingsGroup);
 
         primaryStage.setScene(new Scene(root));
-
         primaryStage.show();
     }
 
@@ -58,14 +89,27 @@ public class Game extends Application {
     }
 
     private void drawGameStep(GraphicsContext gc) {
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
+        for (int i = 0; i < getFieldWidth(); i++) {
+            for (int j = 0; j < getFieldHeight(); j++) {
                 boolean isAlive = this.map.getCell(i, j);
                 gc.setFill(isAlive ? Color.GREEN : Color.GRAY);
-                gc.fillRect(i * 5, j * 5, 5, 5);
+                int cellSize = getCellSize();
+                gc.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
             }
         }
         this.map.nextState();
+    }
+
+    private int getCellSize() {
+        return 5;
+    }
+
+    private int getFieldWidth() {
+        return this.width;
+    }
+
+    private int getFieldHeight() {
+        return this.height;
     }
 
 }
