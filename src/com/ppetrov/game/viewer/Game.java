d5d741f38cf76d69628fe8b54a8eade1854c3109
Pavel Petrov <petrov.pavel.89@gmail.com>
2016-03-27 14:47:02 +0500

    Fixed a bug with drawing cells.

--- src/com/ppetrov/game/viewer/Game.java
+++ src/com/ppetrov/game/viewer/Game.java
@@ -150,11 +150,12 @@ public class Game extends Application {
         gc.setFill(Color.GRAY);
         gc.fillRect(0, 0, this.canvas.getWidth(), this.canvas.getHeight());
 
-        for (int i = 0; i < getFieldWidth(); i++) {
-            for (int j = 0; j < getFieldHeight(); j++) {
+        for (int i = 0; i < getFieldHeight(); i++) {
+            for (int j = 0; j < getFieldWidth(); j++) {
                 drawCalculatedCell(gc, i, j);
             }
         }
+
         drawCellsUnderCursor(gc);
     }
 
