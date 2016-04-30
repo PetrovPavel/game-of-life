21a293ea81ec3dbf171eda8230e2dc6f80832bc1
Pavel Petrov <petrov.pavel.89@gmail.com>
2016-05-01 00:46:24 +0500

    Added more rule templates.

--- src/main/java/com/ppetrov/game/model/RuleTemplate.java
+++ src/main/java/com/ppetrov/game/model/RuleTemplate.java
@@ -2,10 +2,13 @@ package com.ppetrov.game.model;
 
 public enum RuleTemplate {
 
-    DEFAULT("Default", new Rules(new int[]{3}, new int[]{2, 3})),
+    DEFAULT("Life", new Rules(new int[]{3}, new int[]{2, 3})),
     SEEDS("Seeds", new Rules(new int[]{2}, new int[]{})),
     LIFE_34("34 Life", new Rules(new int[]{3, 4}, new int[]{3, 4})),
-    DIAMOEBA("Diamoeba", new Rules(new int[]{3, 5, 6, 7, 8}, new int[]{5, 6, 7, 8}));
+    DIAMOEBA("Diamoeba", new Rules(new int[]{3, 5, 6, 7, 8}, new int[]{5, 6, 7, 8})),
+    TWO_X_TWO("2x2", new Rules(new int[]{3, 6}, new int[]{1, 2, 5})),
+    HIGH_LIFE("High Life", new Rules(new int[]{3, 6}, new int[]{2, 3})),
+    DAY_AND_NIGHT("Day & Night", new Rules(new int[]{3, 6, 7, 8}, new int[]{3, 4, 6, 7, 8}));
 
     private String name;
     private Rules rules;
