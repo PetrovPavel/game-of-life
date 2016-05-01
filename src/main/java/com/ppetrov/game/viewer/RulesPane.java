c70f1bc0996746bf8145ab1814e35711b6c1e2ca
Pavel Petrov <petrov.pavel.89@gmail.com>
2016-05-02 00:36:59 +0500

    Moved templates label to another pane.

--- src/main/java/com/ppetrov/game/viewer/RulesPane.java
+++ src/main/java/com/ppetrov/game/viewer/RulesPane.java
@@ -28,8 +28,7 @@ public class RulesPane extends VBox {
         subscribeOnTemplatesChanges();
         subscribeOnRulesChanges();
 
-        getChildren().addAll(this.bornSurvivesVBox,
-                new Label("Templates:"), this.templatesPane);
+        getChildren().addAll(this.bornSurvivesVBox, this.templatesPane);
     }
 
     private void createBornSurvivesPane() {
@@ -50,7 +49,7 @@ public class RulesPane extends VBox {
         this.templatesPane = new ScrollPane();
         this.templatesPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
 
-        this.templatesVBox = new VBox();
+        this.templatesVBox = new VBox(new Label("Templates:"));
         this.templatesPane.setFitToWidth(true);
         this.templatesGroup = new ToggleGroup();
 
